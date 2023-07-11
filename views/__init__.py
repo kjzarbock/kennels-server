47ce662cb2ba6c379577be598ebf220936155294
Katharine Zarbock <kjzarbock@gmail.com>
2023-07-11 00:25:19 -0500

    added customer, employee location POST

--- views/__init__.py
+++ views/__init__.py
@@ -1,1 +1,13 @@
 from .animal_requests import get_all_animals
+from .animal_requests import get_single_animal
+from .customer_requests import get_all_customers
+from .customer_requests import get_single_customer
+from .employee_requests import get_all_employees
+from .employee_requests import get_single_employee
+from .location_requests import get_all_locations
+from .location_requests import get_single_location
+from .animal_requests import create_animal
+from .customer_requests import create_customer
+from .employee_requests import create_employee
+from .location_requests import create_location
+

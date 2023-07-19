70c887892ff0ca889fc00232a8993d315f87df6d
Katharine Zarbock <kjzarbock@gmail.com>
2023-07-18 22:51:10 -0500

    7/19/23 1504, joined tables for animal/location, animal/customer

--- models/animal.py
+++ models/animal.py
@@ -10,3 +10,5 @@ class Animal():
         self.breed = breed
         self.customer_id = customer_id
         self.location_id = location_id
+        self.location = None
+        self.customer = None
--- views/animal_requests.py
+++ views/animal_requests.py
@@ -1,6 +1,8 @@
 import sqlite3
 import json
 from models import Animal
+from models import Location
+from models import Customer
 
 ANIMALS = [
     {
@@ -8,24 +10,24 @@ ANIMALS = [
         "name": "Snickers",
         "status": "Admitted",
         "breed": "Dog",
-        "location_id": 1,
         "customer_id": 4,
+        "location_id": 1
     },
     {
         "id": 2,
         "name": "Roman",
         "status": "Admitted",
         "breed": "Dog",
-        "location_id": 1,
         "customer_id": 2,
+        "location_id": 1
     },
     {
         "id": 3,
         "name": "Blue",
         "status": "Admitted",
         "breed": "Cat",
-        "location_id": 2,
         "customer_id": 1,
+        "location_id": 2
     }
 ]
 
@@ -46,8 +48,17 @@ def get_all_animals():
             a.breed,
             a.status,
             a.location_id,
-            a.customer_id
-        FROM animal a
+            a.customer_id,
+            l.name location_name,
+            l.address location_address,
+            c.name customer_name,
+            c.address customer_address,
+            c.email customer_email
+        FROM Animal a
+        JOIN Location l
+            ON l.id = a.location_id
+        JOIN Customer c
+            ON c.id = a.customer_id
         """)
 
         # Initialize an empty list to hold all animal representations
@@ -58,15 +69,21 @@ def get_all_animals():
 
         # Iterate list of data returned from database
         for row in dataset:
+            # Create an animal instance from the current row
+            animal = Animal(row['id'], row['name'], row['breed'], row['status'],
+                            row['location_id'], row['customer_id'])
 
-            # Create an animal instance from the current row.
-            # Note that the database fields are specified in
-            # exact order of the parameters defined in the
-            # Animal class above.
-            animal = Animal(row['id'], row['name'], row['breed'],
-                            row['status'], row['location_id'],
-                            row['customer_id'])
+            # Create a Location instance from the current row
+            location = Location(row['location_id'], row['location_name'], row['location_address'])
 
+            # Create a Customer instance from the current row
+            customer = Customer(row['customer_id'], row['customer_name'], row['customer_address'], row['customer_email'])
+
+            # Add the dictionary representation of the location to the animal
+            animal.location = location.__dict__
+            animal.customer = customer.__dict__
+
+            # Add the dictionary representation of the animal to the list
             animals.append(animal.__dict__)
 
     return animals
